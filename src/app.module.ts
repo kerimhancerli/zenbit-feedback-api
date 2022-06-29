905fd353b65d1c610a0e93eac28c779b7f3e6883
kerimhancerli <kerimhancerli34@gmail.com>
2022-06-29 23:49:27 +0300

    db updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,12 +8,12 @@ import { FeedbackModule } from './feedback.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123456',
-      database: 'feedback',
+      type: 'postgres',
+      host: 'ec2-52-30-159-47.eu-west-1.compute.amazonaws.com',
+      port: 5432,
+      username: 'tmajdlpwgztgzp',
+      password: 'c861fa882d79f5e9ec09f48bebbf5892e79ccae4b141af2e65fb7bf8e3df8559',
+      database: 'dd6vfmifs6foj8',
       entities: [Feedback],
       synchronize: true,
       migrationsTableName: 'migrations',
